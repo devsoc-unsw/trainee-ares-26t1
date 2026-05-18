f40d32448f4060692df8cbb965c84adb74cdcf44
Thomas Guan <thomas@NASA-PC.localdomain>
2026-05-18 20:28:24 +1000

    created deleteTask route and logic

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -1,84 +1,42 @@
 import { Response } from "express";
-import { CustomVariant, TaskType } from "../constants/taskConstants";
-import DailyTask from "../classes/task/DailyTask";
-import WeeklyTask from "../classes/task/WeeklyTask";
-import EasyTask from "../classes/task/EasyTask";
-import MediumTask from "../classes/task/MediumTask";
-import HardTask from "../classes/task/HardTask";
+import { createTaskEntry } from "./helpers/taskHelper";
 
 export const createTask = async (req: any, res: Response) => {
   try {
     const { name, type } = req.body;
     const user = req.user;
 
-    let task = null;
-    let taskEntry = {};
-    switch (type) {
-      case TaskType.Daily: {
-        task = new DailyTask(name);
-        taskEntry = {
-          id: task.getId(),
-          name: task.getName(),
-          type: task.getType(),
-          amount: task.getAmount(),
-        };
-        break;
-      }
-
-      case TaskType.Weekly: {
-        const { dayOfWk, deadline } = req.body;
-        task = new WeeklyTask(name, dayOfWk, deadline);
-        taskEntry = {
-          id: task.getId(),
-          name: task.getName(),
-          type: task.getType(),
-          amount: task.getAmount(),
-          dayOfWk: task.getDayOfWk(),
-          deadline: task.getDeadline(),
-        };
-        break;
-      }
-
-      case TaskType.Custom: {
-        const { difficulty, deadline } = req.body;
-        switch (difficulty) {
-          case CustomVariant.Easy: {
-            task = new EasyTask(name, deadline);
-            break;
-          }
-
-          case CustomVariant.Medium: {
-            task = new MediumTask(name, deadline);
-            break;
-          }
-
-          case CustomVariant.Hard: {
-            task = new HardTask(name, deadline);
-            break;
-          }
-
-          default:
-            return res.status(400).json({ error: "Invalid difficulty" });
-        }
-
-        taskEntry = {
-          id: task.getId(),
-          name: task.getName(),
-          type: task.getType(),
-          amount: task.getAmount(),
-          difficulty: task.getDifficulty(),
-          deadline: task.getDeadline(),
-        }
-      }
-
-      default:
-        return res.status(400).json({ error: "Invalid task type" });
+    const taskEntry = createTaskEntry(req, name, type);
+    if ("error" in taskEntry) {
+      return res.status(400).json(taskEntry);
     }
 
+    // Append task to user and update db
     user.tasks.push(taskEntry);
     await user.save();
-    return res.status(201).json({ taskEntry });
+    return res.status(201).json(taskEntry);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
+export const deleteTask = async (req: any, res: Response) => {
+  try {
+    const { taskId } = req.body;
+    const user = req.user;
+
+    // Find the index where the to-be-deleted task i
+    const taskIndex = user.tasks.findIndex(
+      (currTask: any) => currTask.id === taskId,
+    );
+    if (!taskIndex) {
+      return res.status(403).json({ error: "Task does not belong to user" });
+    }
+
+    // Find task from user and remove then update db.
+    user.tasks.splice(taskIndex, 1);
+    await user.save();
+    return res.status(200).json({});
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
--- backend/src/routes/task.ts
+++ backend/src/routes/task.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createTask } from "../controllers/taskController";
+import { createTask, deleteTask } from "../controllers/taskController";
+import { authMiddleware } from "../controllers/helpers/authHelper";
 
 const router = express.Router();
 
-router.post("/create", createTask);
+router.post("/create", authMiddleware, createTask);
+router.delete("/delete", authMiddleware, deleteTask);
 
 export default router;
